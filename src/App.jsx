2dfd02633b3d2d9486ba9fa148dca38bbd9a1211
vinayak-vayuz <vinayak.tripathi@vayuz.com>
2025-09-17 13:10:04 +0530

    changes in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,26 @@ import { ThemeProvider } from "./components/theme";
 import { OnlineStatus } from "./components/onlineStatus";
 import { ScrollToTopButton } from "./components/scrollToTop";
 
+import React from "react";
+import * as MaterialUI from "@mui/material";
+import ReactDOM from "react-dom";
+import * as EmotionReact from "@emotion/react";
+import * as EmotionStyled from "@emotion/styled";
+import * as MuiIcons from "@mui/icons-material";
+import * as ReactRouterDOM from "react-router-dom";
+import * as ReactHotToast from "react-hot-toast";
 
 function App() {
-
+  window.EmotionReact = EmotionReact;
+  window.EmotionStyled = EmotionStyled;
+  window.MuiIcons = MuiIcons;
+  window.process = { env: {} };
+  window.React = React;
+  window.ReactDOM = ReactDOM;
+  window.MaterialUI = MaterialUI;
+  window.ReactRouterDOM = ReactRouterDOM;
+  window.ReactHotToast = ReactHotToast;
+  
   return (
     <>
       <Provider store={store}>
--- src/components/MFloader/MicroFrontendLoader.jsx
+++ src/components/MFloader/MicroFrontendLoader.jsx
@@ -1,111 +1,3 @@
-// import React, {
-//   useEffect,
-//   useRef,
-//   forwardRef,
-//   useImperativeHandle,
-//   useState,
-// } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// const MicrofrontendLoader = forwardRef(
-//   ({ scriptUrl, cssUrl, mountDivId, globalVarName, propsToPass }, ref) => {
-//     const rootRef = useRef(null);
-//     const instanceRef = useRef(null);
-//     const containerRef = useRef(null);
-//     const linkRef = useRef(null);
-
-//     const [loading, setLoading] = useState(true);
-
-//     useEffect(() => {
-//       // Load CSS first (optional)
-//       if (cssUrl) {
-//         const link = document.createElement("link");
-//         link.rel = "stylesheet";
-//         link.href = cssUrl;
-//         link.type = "text/css";
-//         linkRef.current = link;
-//         document.head.appendChild(link);
-//       }
-
-//       const script = document.createElement("script");
-//       script.src = scriptUrl;
-//       script.async = true;
-//       document.body.appendChild(script);
-
-//       script.onload = () => {
-//         const Component = window[globalVarName];
-//         if (Component && containerRef.current) {
-//           if (!rootRef.current) {
-//             rootRef.current = window.ReactDOM.createRoot(containerRef.current);
-//             instanceRef.current = {
-//               props: propsToPass,
-//               render: (newProps) => {
-//                 instanceRef.current.props = {
-//                   ...instanceRef.current.props,
-//                   ...newProps,
-//                 };
-//                 rootRef.current.render(
-//                   window.React.createElement(
-//                     Component,
-//                     instanceRef.current.props
-//                   )
-//                 );
-//               },
-//             };
-//             instanceRef.current.render({});
-//             setLoading(false);
-//           }
-//         }
-//       };
-
-//       return () => {
-//         if (rootRef.current) {
-//           rootRef.current.unmount();
-//           rootRef.current = null;
-//         }
-//         if (containerRef.current) {
-//           containerRef.current.innerHTML = "";
-//         }
-//         if (script.parentNode) {
-//           document.body.removeChild(script);
-//         }
-//         if (linkRef.current && linkRef.current.parentNode) {
-//           document.head.removeChild(linkRef.current);
-//         }
-//       };
-//     }, [scriptUrl, cssUrl, mountDivId, globalVarName]); // no rerun on props
-
-//     // Provide method to update props from outside
-//     useImperativeHandle(ref, () => ({
-//       updateProps: (newProps) => {
-//         if (instanceRef.current) {
-//           instanceRef.current.render(newProps);
-//         }
-//       },
-//     }));
-
-//     const navigate = useNavigate();
-
-//     return (
-//       <div>
-//         {loading && (
-//           <div style={{ textAlign: "center", padding: "1rem" }}>
-//             Loading Microfrontend...
-//           </div>
-//         )}
-//         <div
-//           id={mountDivId}
-//           ref={containerRef}
-//           style={{ display: loading ? "none" : "block" }}
-//         />
-//       </div>
-//     );
-//   }
-// );
-
-// export default MicrofrontendLoader;
-
-
 import React, {
   useEffect,
   forwardRef,
@@ -133,9 +25,12 @@ const MicrofrontendLoader = forwardRef(
       }
 
       script.onload = () => {
+        console.log('loaaddd')
         const RemoteComponent = window[globalVarName];
         if (onLoad) onLoad();
         if (RemoteComponent) {
+          console.log('loaaddd 2')
+
           setComponent(() => RemoteComponent);
         } else {
           console.error(`Global var ${globalVarName} not found`);
