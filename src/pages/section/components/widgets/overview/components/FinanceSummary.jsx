e5d1fd17c4c64318052a7bbb7a5daf18d2e8f5a5
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-09-25 16:53:31 +0530

    changes

--- src/pages/section/components/widgets/overview/components/FinanceSummary.jsx
+++ src/pages/section/components/widgets/overview/components/FinanceSummary.jsx
@@ -30,23 +30,23 @@ function FinanceSummary({  financial,data  }) {
       title="Income"
       period="This Month"
       icon={<LuReceiptText className="text-2xl text-green-600" />}
-      className="mb-4 break-inside-avoid h-[232px] overflow-hidden"
+      className="!gap-4 break-inside-avoid h-[232px] overflow-hidden"
     >
-      <div className="grid grid-cols-4 text-xs text-gray-500 mb-2">
+      <div className="grid grid-cols-4 text-xs text-[#64748B] mb-2">
         <div>Opening</div>
         <div>Income</div>
         <div>Collection</div>
         <div>Closing</div>
       </div>
 
-      <div className="grid grid-cols-4 text-xs font-medium text-gray-900 mb-4">
+      <div className="grid grid-cols-4 text-xs font-medium text-[#121212] mb-2">
         <div>{formatCurrency(income.opening_balance)} AED</div>
         <div>{formatCurrency(income.income)} AED</div>
         <div>{formatCurrency(income.collection)} AED</div>
         <div>{formatCurrency(income.closing_balance)} AED</div>
       </div>
 
-      <hr className="my-2 border-t border-dashed border-gray-300" />
+      <hr className="my-4 border-t border-dashed border-gray-300" />
 
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
@@ -64,14 +64,14 @@ function FinanceSummary({  financial,data  }) {
         </div>
       </div>
 
-      <div className="grid grid-cols-4 text-xs text-gray-500 mb-2">
+      <div className="grid grid-cols-4 text-xs text-[#64748B] mb-2">
         <div>Opening</div>
         <div>Expenditure</div>
         <div>Payment</div>
         <div>Outstanding</div>
       </div>
 
-      <div className="grid grid-cols-4 text-xs font-medium text-gray-900">
+      <div className="grid grid-cols-4 text-xs font-medium text-[#121212]">
         <div>{formatCurrency(expenditure.opening_balance)} AED</div>
         <div>{formatCurrency(expenditure.expenditure)} AED</div>
         <div>{formatCurrency(expenditure.payment)} AED</div>
