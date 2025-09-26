e7c3588a58fa9c84062ac840601575d16da6bdbe
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-09-26 14:55:32 +0530

    changes

--- src/pages/section/components/widgets/overview/components/FinanceSummary.jsx
+++ src/pages/section/components/widgets/overview/components/FinanceSummary.jsx
@@ -31,50 +31,52 @@ function FinanceSummary({ financial, data }) {
       icon={<LuReceiptText className="text-2xl text-green-600" />}
       className="!gap-4 break-inside-avoid h-[232px] overflow-hidden"
     >
-      <div className="grid grid-cols-4 text-xs text-[#64748B] mb-2">
-        <div>Opening</div>
-        <div>Income</div>
-        <div>Collection</div>
-        <div>Closing</div>
-      </div>
+      <div>
+        <div className="grid grid-cols-4 text-xs text-[#64748B] mb-2">
+          <div>Opening</div>
+          <div>Income</div>
+          <div>Collection</div>
+          <div>Closing</div>
+        </div>
 
-      <div className="grid grid-cols-4 text-xs font-medium text-[#121212] mb-2">
-        <div>{formatCurrency(income.opening_balance)} AED</div>
-        <div>{formatCurrency(income.income)} AED</div>
-        <div>{formatCurrency(income.collection)} AED</div>
-        <div>{formatCurrency(income.closing_balance)} AED</div>
-      </div>
+        <div className="grid grid-cols-4 text-xs font-medium text-[#121212] mb-2">
+          <div>{formatCurrency(income.opening_balance)} AED</div>
+          <div>{formatCurrency(income.income)} AED</div>
+          <div>{formatCurrency(income.collection)} AED</div>
+          <div>{formatCurrency(income.closing_balance)} AED</div>
+        </div>
 
-      <hr className="my-4 border-t border-dashed border-gray-300" />
+        <hr className="my-4 border-t border-dashed border-gray-300" />
 
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <LuReceiptText className="text-2xl text-red-600" />
-          <div
-            style={{ fontWeight: 500, fontSize: "16px", lineHeight: "20px" }}
-            className="!my-0 !font-medium !text-[16px] !leading-[20px] !text-[#121212]"
-          >
-            Expenditure
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <LuReceiptText className="text-2xl text-red-600" />
+            <div
+              style={{ fontWeight: 500, fontSize: "16px", lineHeight: "20px" }}
+              className="!my-0 !font-medium !text-[16px] !leading-[20px] !text-[#121212]"
+            >
+              Expenditure
+            </div>
+          </div>
+
+          <div className="!text-xs !leading-[16px] !text-[#64748B]">
+            This Month
           </div>
         </div>
 
-        <div className="!text-xs !leading-[16px] !text-[#64748B]">
-          This Month
+        <div className="grid grid-cols-4 text-xs text-[#64748B] mt-4 mb-2">
+          <div>Opening</div>
+          <div>Expenditure</div>
+          <div>Payment</div>
+          <div>Outstanding</div>
         </div>
-      </div>
 
-      <div className="grid grid-cols-4 text-xs text-[#64748B] mb-2">
-        <div>Opening</div>
-        <div>Expenditure</div>
-        <div>Payment</div>
-        <div>Outstanding</div>
-      </div>
-
-      <div className="grid grid-cols-4 text-xs font-medium text-[#121212]">
-        <div>{formatCurrency(expenditure.opening_balance)} AED</div>
-        <div>{formatCurrency(expenditure.expenditure)} AED</div>
-        <div>{formatCurrency(expenditure.payment)} AED</div>
-        <div>{formatCurrency(expenditure.outstanding)} AED</div>
+        <div className="grid grid-cols-4 text-xs font-medium text-[#121212]">
+          <div>{formatCurrency(expenditure.opening_balance)} AED</div>
+          <div>{formatCurrency(expenditure.expenditure)} AED</div>
+          <div>{formatCurrency(expenditure.payment)} AED</div>
+          <div>{formatCurrency(expenditure.outstanding)} AED</div>
+        </div>
       </div>
     </Card>
   );
