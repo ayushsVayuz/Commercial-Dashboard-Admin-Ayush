cca108b4281f89bef0e9fbfde5a270aa97b6a2f6
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-09-26 16:53:57 +0530

    changes

--- src/pages/section/components/widgets/community/component/ResidentOverviewCard.jsx
+++ src/pages/section/components/widgets/community/component/ResidentOverviewCard.jsx
@@ -81,15 +81,14 @@ const ResidentOverviewCard = ({
       title="Resident Overview"
       icon={<GoHome className="h-6 w-6 text-[#8B5CF6]" />}
       className="h-[238px]"
-      childrenClassName="flex-1 min-h-0"
     >
-      <div className="grid grid-cols-12 gap-4 items-center">
+      <div className=" flex-1 min-h-0 grid grid-cols-12 gap-4 items-center">
         <div className="col-span-5">
           <div>
             <div className="text-[12px] text-[#64748B] leading-[16px]">
               Total Owners
             </div>
-            <div className="text-[28px] leading-[32px ] font-semibold text-[#3C81F6]">
+            <div className="text-[28px] leading-[32px ]  text-[#3C81F6]">
               {ownerCount}
             </div>
           </div>
@@ -97,13 +96,13 @@ const ResidentOverviewCard = ({
             <div className="text-[12px] text-[#64748B]  leading-[16px]">
               Total Tenants
             </div>
-            <div className="text-[28px] leading-[32px ] font-semibold text-[#08B6D4]">
+            <div className="text-[28px] leading-[32px ] text-[#08B6D4]">
               {tenantCount}
             </div>
           </div>
-          <div className="text-xs text-[#1FA05B] font-medium flex gap-2">
+          <div className="text-xs text-[#1FA05B] flex gap-2">
             {growthPct}{" "}
-            <div className="text-[#64748B] font-normal">
+            <div className="text-[#64748B] text-[10px]">
               Compared to last month
             </div>
           </div>
