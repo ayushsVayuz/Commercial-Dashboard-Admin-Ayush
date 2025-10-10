a55c1095f31f7014ff65fb6cf5b10e4b88a1c078
vinayaktripathi-in <vinayaktripathi@hotmail.com>
2025-10-10 13:06:32 +0530

    changes

--- src/redux/actions/encryption-action.js
+++ src/redux/actions/encryption-action.js
@@ -4,12 +4,12 @@ import client from "../axios-baseurl";
 export const readPublicKey = createAsyncThunk(
   "encryption/readPublicKey",
   async (_, { rejectWithValue, getState }) => {
-    const token = getAuthToken(getState);
+    // const token = getAuthToken(getState);
     try {
       const response = await client.get("/keys/public-key", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+        // headers: {
+        //   Authorization: `Bearer ${token}`,
+        // },
       });
       return response.data;
     } catch (error) {
