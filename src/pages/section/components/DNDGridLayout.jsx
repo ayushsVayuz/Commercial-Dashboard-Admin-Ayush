7149ffb672601d554381fe9750f6fb56545222c9
Ayush Sharma <ayushs.sharma@vayuz.com>
2026-02-10 14:33:51 +0530

    Changes

--- src/pages/section/components/DNDGridLayout.jsx
+++ src/pages/section/components/DNDGridLayout.jsx
@@ -154,7 +154,7 @@ export default function DNDGridLayout({
   value,
   onChange,
   data,
-  isResizable = true,
+  isResizable = false,
   isDraggable = true,
   widgetPositions,
   setWidgetPositions,
